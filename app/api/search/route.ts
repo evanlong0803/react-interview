7a0ac8b7a3c469bcc592466e642be4573782185a
Cuvii <i@cuvii.dev>
2025-02-25 11:39:32 +0800

    fix: build

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextRequest, NextResponse } from 'next/server'
 
 const searchRelayUrl = 'https://api.github.com/search/repositories'
